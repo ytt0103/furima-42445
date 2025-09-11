a60a0265febb5996722217bc1983122045812903
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-09-11 22:04:13 +0900

    7 commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  belongs_to :user # この行を追加
+  has_many :item_categories # この行を追加
+  has_many :categories, through: :item_categories # この行を追加
 end
--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,4 +1,4 @@
 class ItemCategory < ApplicationRecord
-  belongs_to :item
-  belongs_to :category
+  belongs_to :item # この行を追加
+  belongs_to :category # この行を追加
 end
