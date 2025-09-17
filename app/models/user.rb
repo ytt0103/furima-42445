50af91b44000a59ef992b7ecaafed3d0515a8e41
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-09-17 21:53:19 +0900

    Revert "13 commit"
    This reverts commit 6f3f4b0778cdda39db0c3a194ea603f48cf718ad.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :name, presence: true # この行をコメントアウト
+  validates :name, presence: true # この行を追加
   validates :email, presence: true, uniqueness: { case_sensitive: false } # この行を追加
   validates :password, length: { minimum: 6 } # この行を追加
   validates :nickname, presence: true # この行を追加
