e10ee6dd4b283ccef486240a0f329739852602ca
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-09-21 15:13:18 +0900

    u 3 commit

--- db/migrate/20250921055121_devise_create_users.rb
+++ db/migrate/20250921055121_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name_kana,     null: false, default: ""
+      t.string :first_name_kana,    null: false, default: ""
+      t.string :birth_date,         null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
