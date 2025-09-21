eeb68b9685339a6a06404b7225d2956483ba165f
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-09-21 16:43:12 +0900

    u 4 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true # この行を追加
+  #validates :password, # この行を追加
 end
