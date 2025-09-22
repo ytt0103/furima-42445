c9395dfd1060ccdff9cb619a593ed5016ff60eb7
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-09-23 03:58:01 +0900

    u 8 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :nickname
-    validates :birth_date
-  end
+  #has_many :items # この行を追加
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' } do
-    validates :last_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角（カタカナ）で入力してください' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :nickname, presence: true # この行を追加
+  #validates :password, # この行を追加
 end
