2a4e19450c5e9d47407df605b84fb2f65c57b6d1
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-09-30 22:07:44 +0900

    出品 3commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
-  def index
+  # ログイン状態のチェック (New!)
+  before_action :authenticate_user!, only: [:new, :create] 
 
-    
+  def index
+    @items = Item.all 
+  end
+
+  def new # このアクションを追加
+    @item = Item.new # 空のItemオブジェクト（インスタンス）を作成
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "items#index"
+  # root 'items#index' # 既存の行（もしあればコメントアウトするか削除）
+
+  root 'items#index' # 追加または修正: トップページをitems#indexに設定
+
+  resources :items, only: [:index, :new, :create] # この行を追加
 end
