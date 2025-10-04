5f8a53e97a1e38e86d05ce49b8b254fd0cab55cb
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-10-04 22:09:22 +0900

    出品 5commit

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,112 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  # 💡ユーザーの意向を尊重し、元のpending行をそのまま残します。
   pending "add some examples to (or delete) #{__FILE__}"
+
+  # テスト開始前に実行される。Itemの有効なインスタンスを作成する。
+  before do
+    @item = FactoryBot.build(:item)
+    # 補足: @itemを定義するbeforeブロックは、pending行よりも下でも問題なく動作します。
+  end
+
+  describe '商品出品' do
+    context '出品できるとき' do
+      it 'すべての項目が正しく入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できないとき' do
+      it '商品画像がないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '商品名が空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it '商品の説明が空では出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'カテゴリーが「---」では出品できない' do
+        @item.category_id = 1 # ActiveHashのID:1は「--」を想定
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態が「---」では出品できない' do
+        @item.item_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status can't be blank")
+      end
+
+      it '配送料の負担が「---」では出品できない' do
+        @item.shipping_fee_payer_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee payer can't be blank")
+      end
+
+      it '発送元の地域が「---」では出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送までの日数が「---」では出品できない' do
+        @item.delivery_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+
+      it '価格が空では出品できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '価格が300円未満では出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it '価格が10,000,000円以上では出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it '価格が全角文字では出品できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it '価格が半角英数混合では出品できない' do
+        @item.price = '300yen'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+      
+      it '価格が半角英字だけでは出品できない' do
+        @item.price = 'threethousand'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it 'ユーザーが紐付いていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
