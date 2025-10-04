10d8df96bcfb23071d04377562abe8fc0ba12159
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-10-04 14:27:55 +0900

    出品 4commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,36 @@
 class ItemsController < ApplicationController
-  # ログイン状態のチェック (New!)
   before_action :authenticate_user!, only: [:new, :create] 
 
   def index
-    @items = Item.all 
+    @items = Item.all
   end
 
-  def new # このアクションを追加
-    @item = Item.new # 空のItemオブジェクト（インスタンス）を作成
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params) # 1. フォームのデータを受け取り、新しいItemオブジェクトを作る
+    if @item.save # 2. データベースに保存を試みる
+      redirect_to root_path # 3. 保存が成功したらトップページへ移動
+    else
+      render :new, status: :unprocessable_entity # 4. 保存が失敗したら、エラーメッセージ付きで出品ページを再表示
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image, 
+      :name, 
+      :description, 
+      :category_id, 
+      :item_status_id, 
+      :shipping_fee_payer_id, 
+      :prefecture_id, 
+      :delivery_day_id, 
+      :price
+    ).merge(user_id: current_user.id) # 5. ログインしているユーザーのIDを商品データに加える
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,5 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    price { 1 }
-    user { nil }
-    category_id { 1 }
-    item_status_id { 1 }
-    shipping_fee_payer_id { 1 }
-    prefecture_id { 1 }
-    delivery_day_id { 1 }
+    
   end
 end
