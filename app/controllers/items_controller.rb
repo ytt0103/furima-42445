09583d92dce088b84f573b904b0d6e910b8b2048
ytt0103 <0vg7-15m385187n@ezweb.ne.jp>
2025-10-09 21:30:06 +0900

    出品 10 commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] 
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  #before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all
   end
 
   def new
